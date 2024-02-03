e25353ec34de9b785a52b7dafb314e8926f9c50f
Sina Siadat <siadat@gmail.com>
2024-02-03 15:03:40 +0000

    add test comment
    [ghstack-poisoned]

--- day1/main.zig
+++ day1/main.zig
@@ -6,6 +6,7 @@ pub fn main() !void {
     var buf = std.ArrayList(u8).init(std.heap.page_allocator);
     defer buf.deinit();
 
+    // add some comment
     const count = try stdin.readAllArrayList(&buf, 22110);
     try stdout.print("read {} bytes, they are {any}", .{ count, buf.items });
 }
