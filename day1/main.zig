6027e3950c519f9cf1ed740bdda923fb7560d356
Sina Siadat <siadat@gmail.com>
2024-02-04 13:17:28 +0000

    my first-ever zig struct

--- day1/main.zig
+++ day1/main.zig
@@ -3,21 +3,45 @@ const stdout = std.io.getStdOut().writer();
 const stdin = std.io.getStdIn().reader();
 const assert = std.debug.assert;
 
+const Calculator = struct {
+    total_sum: u64 = 0,
+    line_first_num: u64 = 0,
+    line_last_num: u64 = 0,
+
+    pub fn handleByte(self: *Calculator, byte: u8) !void {
+        if (byte == '\n') {
+            try self.handleEndOfLine();
+        } else if (byte >= '1' and byte <= '9') {
+            self.handleNumber(byte);
+        } else {
+            // no op
+        }
+    }
+    pub fn handleNumber(self: *Calculator, byte: u8) void {
+        const value = byte - '1' + 1;
+        if (self.line_first_num == 0) {
+            self.line_first_num = value;
+        }
+        self.line_last_num = value;
+    }
+    pub fn handleEndOfLine(self: *Calculator) !void {
+        try stdout.print("{}+{}\n\n", .{ self.line_first_num, self.line_last_num });
+        self.total_sum += 10 * self.line_first_num + self.line_last_num;
+        self.line_first_num = 0;
+        self.line_last_num = 0;
+    }
+};
+
 pub fn main() !void {
     var buf = std.ArrayList(u8).init(std.heap.page_allocator);
     defer buf.deinit();
 
-    var total_sum: u64 = 0;
-    var line_first_num: u64 = 0;
-    var line_last_num: u64 = 0;
+    var calculator = Calculator{};
 
     while (true) {
         const byte = stdin.readByte() catch |err| switch (err) {
             error.EndOfStream => {
-                try stdout.print("{}+{}\n\n", .{ line_first_num, line_last_num });
-                total_sum += 10 * line_first_num + line_last_num;
-                line_first_num = 0;
-                line_last_num = 0;
+                try calculator.handleEndOfLine();
                 break;
             },
             else => {
@@ -25,22 +49,9 @@ pub fn main() !void {
                 return err;
             },
         };
-
-        try stdout.print("{c}:{}\n", .{ byte, byte });
-
-        if (byte == '\n') {
-            try stdout.print("{}+{}\n\n", .{ line_first_num, line_last_num });
-            total_sum += 10 * line_first_num + line_last_num;
-            line_first_num = 0;
-            line_last_num = 0;
-        } else if (byte >= '1' and byte <= '9') {
-            const value = byte - '1' + 1;
-            if (line_first_num == 0) {
-                line_first_num = value;
-            }
-            line_last_num = value;
-        }
+        try stdout.print("{c}", .{byte});
+        try calculator.handleByte(byte);
     }
-    try stdout.print("total_sum: {}\n", .{total_sum});
-    assert(54940 == total_sum);
+    try stdout.print("total_sum: {}\n", .{calculator.total_sum});
+    assert(54940 == calculator.total_sum);
 }
