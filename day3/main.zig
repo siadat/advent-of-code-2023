dac042f45f0fdd2d03dfd75c639c2023e482b58d
Sina Siadat <siadat@gmail.com>
2024-02-19 18:08:19 +0000

    remove old comments

--- day3/main.zig
+++ day3/main.zig
@@ -155,36 +155,6 @@ const Solver = struct {
 };
 
 pub fn main() !void {
-    // TODO:
-
-    // First line (i==0):
-    //   lineA = lines[0]
-    //   Check all symbols on lineA and match with numbers on lineA
-    // Take 2 lines at a time
-    //   lineA = lines[i]
-    //   lineB = lines[i+1]
-    //   Check all symbols on lineA and match with numbers on lineB
-    //   Check all symbols on lineB and match with numbers on lineA and lineB
-
-    // Alternative approach:
-    // Take 2 lines at a time
-    //   lineA = lines[i]
-    //   lineB = lines[i+1]
-    //   Check all symbols (or numbers) on lineA and match with numbers (or symbols) on lineA
-    //   Check all symbols on lineA and match with numbers on lineB
-    //   Check all numbers on lineA and match with numbers on lineB
-
-    // Alternative approach (one extra line in memory, but simpler):
-    // Take 3 lines at a time
-    //   lineA = lines[i]
-    //   lineB = lines[i+1]
-    //   lineC = lines[i+2]
-    //   Check all symbols on lineB and match with numbers on lineA and lineC
-
-    // Alternative approach (keep one single line in memoery, simple):
-    // Read line[n] one byte at a time
-    // Read line[n+1] one byte at a time and match each symbole or number with number or symboles in line[n] and update the same array after match is done, might have to do some acrobatics to make sure we don't overwrite a symbole or number too early. I can switch to one of the simpler approaches depending on how it unfolds.
-    // repeat
     var gpa = std.heap.GeneralPurposeAllocator(.{}){};
     defer {
         const deinit_status = gpa.deinit();
